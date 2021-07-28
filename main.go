08f42794a09c36bc6f9cf0464d9a7e250e4c7ec9
Xarakipi <kungurovartur@gmail.com>
2021-07-28 09:59:40 +0300

    test jenkins pipline

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, World 2! Again...")
+    fmt.Fprintf(w, "Hello, World 2! Again...2")
 }
 
 func main() {
